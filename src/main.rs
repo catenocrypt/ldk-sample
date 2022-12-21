0af17b766150e22f63e34892a07d4610e42f8286
catenocrypt <13562139+catenocrypt@users.noreply.github.com>
2022-12-21 14:41:25 +0100

    Minor cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 pub mod bitcoind_client;
 
-pub mod wc_proto;
 pub mod wallet;
 pub mod env;
 
@@ -208,26 +207,28 @@ async fn handle_ldk_events(
 				encode::deserialize(&hex_utils::to_vec(&signed_tx.hex).unwrap()).unwrap();
 			*/
 
-
 			// Create transaction
-			let wc_tx = wallet.create_send_tx(addr.as_str(), *channel_value_satoshis);
-			let final_wc_tx: Transaction = encode::deserialize(&wc_tx).unwrap();
+			match wallet.create_send_tx(addr.as_str(), *channel_value_satoshis) {
+				Err(e) => println!("Error creating tx {}", e.to_string()),
+				Ok(tx) => {
+					let final_funding_tx: Transaction = encode::deserialize(&tx).unwrap();
 
-
-			// Give the funding transaction back to LDK for opening the channel.
-			if channel_manager
-				.funding_transaction_generated(
-					&temporary_channel_id,
-					counterparty_node_id,
-					final_wc_tx,
-				)
-				.is_err()
-			{
-				println!(
-					"\nERROR: Channel went away before we could fund it. The peer disconnected or refused the channel.");
-				print!("> ");
-				io::stdout().flush().unwrap();
-			}
+					// Give the funding transaction back to LDK for opening the channel.
+					if channel_manager
+						.funding_transaction_generated(
+							&temporary_channel_id,
+							counterparty_node_id,
+							final_funding_tx,
+						)
+						.is_err()
+					{
+						println!(
+							"\nERROR: Channel went away before we could fund it. The peer disconnected or refused the channel.");
+						print!("> ");
+						io::stdout().flush().unwrap();
+					}
+				}
+			};
 		}
 		Event::PaymentReceived { payment_hash, purpose, amount_msat } => {
 			println!(
